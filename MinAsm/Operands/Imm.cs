aa4d4709bad09cbb57559d31605fc82aff38ad47
Konctantin <gawrilyako@gmail.com>
2016-03-29 23:06:08 +0300

    Immedicate stuff (temporary)
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- MinAsm/Operands/Imm.cs
+++ MinAsm/Operands/Imm.cs
@@ -1,4 +1,5 @@
 ﻿using MinAsm.Encoding;
+using System;
 
 namespace MinAsm.Operands
 {
@@ -49,9 +50,28 @@ namespace MinAsm.Operands
 
         public override int Construct(Context context, Instruction instruction)
         {
+            // todo: more variants
+            instruction.Immedicate = this;
             return OperandSize;
         }
 
         public override string ToString() => $"0x{m_value:X}";
+
+        public byte[] Bytes
+        {
+            get
+            {
+                switch(Size)
+                {
+                    case DataSize.None:  return new byte[0];
+                    case DataSize.Bit8:  return BitConverter.GetBytes((byte)m_value);
+                    case DataSize.Bit16:
+                    case DataSize.Bit32: return BitConverter.GetBytes((int)m_value);
+                    case DataSize.Bit64: return BitConverter.GetBytes(m_value);
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+        }
     }
 }
