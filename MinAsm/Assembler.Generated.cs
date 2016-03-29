b18849b2247f0c2cdb33b2bfc55c559db6224305
Konctantin <gawrilyako@gmail.com>
2016-03-29 22:44:01 +0300

    Append more no operands instructions.
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- MinAsm/Assembler.Generated.cs
+++ MinAsm/Assembler.Generated.cs
@@ -9,10 +9,68 @@ namespace MinAsm
 {
     public abstract partial class Assembler
     {
+        #region No operands opcodes
         /// <summary>
         /// No Operation.
         /// </summary>
         public void Nop() => Add(new I("NOP", 0x00, new O(0x90), 0));
+
+        /// <summary>
+        /// Call to Interrupt Procedure.
+        /// </summary>
+        public void Int3() => Add(new I("INT 3", 0x00, new O(0xCC), 0));
+
+        /// <summary>
+        /// Spin Loop Hint.
+        /// </summary>
+        public void Pause() => Add(new I("PAUSE", 0xF3, new O(0x90), 0));
+
+        /// <summary>
+        /// Return From Procedure.
+        /// </summary>
+        public void RenN() => Add(new I("RETN", 0x00, new O(0xC3), 0));
+
+        /// <summary>
+        /// Return From Procedure.
+        /// </summary>
+        public void RenF() => Add(new I("RETF", 0x00, new O(0xCB), 0));
+
+        /// <summary>
+        /// Set Carry Flag.
+        /// </summary>
+        public void Stc() => Add(new I("STC", 0x00, new O(0xF9), 0));
+
+        /// <summary>
+        /// Set Direction Flag.
+        /// </summary>
+        public void Std() => Add(new I("STD", 0x00, new O(0xFD), 0));
+
+        /// <summary>
+        /// Set Interrupt Flag.
+        /// </summary>
+        public void Sti() => Add(new I("STI", 0x00, new O(0xFB), 0));
+
+        /// <summary>
+        /// Clear Carry Flag.
+        /// </summary>
+        public void Clc() => Add(new I("CLC", 0x00, new O(0xF8), 0));
+
+        /// <summary>
+        /// Clear Direction Flag.
+        /// </summary>
+        public void Cld() => Add(new I("CLD", 0x00, new O(0xFC), 0));
+
+        /// <summary>
+        /// Clear Interrupt Flag. (ring 1)
+        /// </summary>
+        public void Cli() => Add(new I("CLI", 0x00, new O(0xFA), 0));
+
+        /// <summary>
+        /// Complement Carry Flag.
+        /// </summary>
+        public void Cmc() => Add(new I("CMC", 0x00, new O(0xF5), 0));
+
+        #endregion
 
         #region Mov
 
