d83b11c8bef41aa0385c49d062a0d0cf6803d95c
Konctantin <gawrilyako@gmail.com>
2016-03-30 09:14:28 +0300

    Test progtam.
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- MinAsm/Program.cs
+++ MinAsm/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using MinAsm.Operands;
 
 namespace MinAsm
 {
@@ -6,10 +8,25 @@ namespace MinAsm
     {
         static void Main(string[] args)
         {
-            var asm = new Assembler32();
-            asm.Mov(Register.RAX, 0x44556677);
-            asm.Mov(Register.RAX, 0x44556677L);
+            var asm = new Assembler64();
+
+            // mov
+            asm.Mov(Register.RCX, 0x4455667777);
+            asm.Mov(Register.RAX, 0xDDDDDD);
+            asm.Mov(Register.RCX, new EffectiveAddres(Register.RDX));
+            asm.Mov(Register.RCX, Register.RDX);
+
+            //cmp
+            asm.Cmp(Register.RCX, 0xdddddd);
+
+            // simple instructions
+            asm.Pause();
             asm.Nop();
+            asm.RetN();
+            asm.RetN(0x2255);
+
+            Console.WriteLine(string.Join("\n", asm));
+
             Console.ReadLine();
         }
     }
