b274d096e73e7d92f83861f5406a4aa3680a9bfb
Konctantin <gawrilyako@gmail.com>
2016-04-07 07:07:29 +0300

    Add test module
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

---
+++ MinAsm.Tests/RegisterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinAsm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MinAsm.Tests
+{
+    [TestClass]
+    public class RegisterTest
+    {
+        [TestMethod]
+        public void TestRegisterFullValues()
+        {
+            Assert.AreEqual(Register.EAX.Full, 8320);
+        }
+    }
+}
---
+++ MinAsm.Tests/SibTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MinAsm;
+using MinAsm.Encoding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MinAsm.Encoding.Tests
+{
+    [TestClass]
+    public class SibTest
+    {
+        [TestMethod]
+        public void TypeCastByteToSibMethod()
+        {
+            Assert.AreEqual(new Sib(0x55), (Sib)0x55);
+        }
+
+        [TestMethod]
+        public void TypeCastSibToByteMethod()
+        {
+            Assert.AreEqual((byte)new Sib(0x55), 0x55);
+        }
+
+        [TestMethod]
+        public void PropertyBaseTestMethod()
+        {
+            Assert.AreEqual<byte>(new Sib(0x55).Base, 5);
+        }
+
+        [TestMethod]
+        public void PropertyScaleTestMethod()
+        {
+            Assert.AreEqual<byte>((byte)new Sib(0x55).Scale, 1);
+        }
+
+        [TestMethod]
+        public void PropertyIndexTestMethod()
+        {
+            Assert.AreEqual<byte>(new Sib(0x55).Index, 2);
+        }
+    }
+}
